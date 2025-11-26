b9f4e1681079f9f2f40b49d145304dccfdae02c5
mohamed mazrou <mohamedmazrou994@gmail.com>
2025-11-26 04:17:37 +0200

    modify interceptors

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,15 +1,19 @@
 import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { url } from '../environment/baseUrl';
 import { NgxSpinnerService, NgxSpinnerModule } from 'ngx-spinner';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { catchError, finalize, tap, throwError } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
    const spinner = inject(NgxSpinnerService)
      const cookies = inject(CookieService)
-      const Token = cookies.get('accessToken')
-
+      const platformId = inject(PLATFORM_ID);
+        let Token: string = '';
+       if (isPlatformBrowser(platformId)) {
+     Token = cookies.get('accessToken')  
+       }
 
   const myReq = req.clone({
      url : url.baseUrl + req.url,
